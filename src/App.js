0b0c730cf7883ea42dec17d2a122b7ee4c358593
alexivanchyk <infosash88@gmail.com>
2023-10-17 19:20:09 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      some xz!
-    </div>
+    <>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+    </>
   );
 }
 
